2dfe47cd2cb142ad46456da89a00cdc446fa03ab
Sam2kx <bandasampathkumar9@gmail.com>
2026-03-10 07:39:47 +0530

    Add files via upload

--- Lion.java
+++ Lion.java
@@ -1,4 +1,5 @@
-class Lion extends Animal1{
+abstract class Lion extends Animal1{
+	abstract void behave();
 	public void sound(){
 		System.out.println("Lion Roaring...");
 	}
--- Tiger.java
+++ Tiger.java
@@ -1,4 +1,7 @@
-class Tiger extends Animal1{
+class Tiger extends Lion{
+	public void behave(){
+		System.out.println("Peek");
+	}
 	public void sound(){
 		System.out.println("Tiger Roaring...");
 	}
